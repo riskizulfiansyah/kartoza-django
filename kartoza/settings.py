6b71c53ae37a8c9b1cc1ac83dafc9ffd29805d9e
Riski Z <riski.zulfiansyah@gmail.com>
2025-03-08 17:58:42 +0700

    feat(map): add leaflet widget

--- kartoza/settings.py
+++ kartoza/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "profiles",
     "django.contrib.gis",
+    "leaflet",
 ]
 
 MIDDLEWARE = [
--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from profiles.models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from leaflet.forms.widgets import LeafletWidget
 
 
 class UserProfileForm(forms.ModelForm):
@@ -18,12 +19,17 @@ class UserProfileForm(forms.ModelForm):
             "phone_number",
             "location",
         ]
+        widgets = {
+            "location": LeafletWidget(attrs={"zoom": 15}),
+        }
 
     def __init__(self, *args, **kwargs):
         request = kwargs.pop("request", None)
         super().__init__(*args, **kwargs)
         if request and not request.user.is_superuser:
             self.fields["user"].widget = forms.HiddenInput()
+        if self.instance.location:
+            self.fields["location"].widget.attrs["initial"] = self.instance.location
 
 
 class UserRegistrationForm(UserCreationForm):
